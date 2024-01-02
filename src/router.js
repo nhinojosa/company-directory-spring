ba5644350d320992baed6e416af7d02304baa8be
nhinojosa <nhinojosa@southtexascollege.edu>
2024-01-02 09:46:43 -0600

    created card details page

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
-
+import CardDetails from '@/views/CardDetails.vue'
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
   { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/employees/:username', name: 'CardDetails', component: () => import('@/views/CardDetails.vue') },
 ]
 
 const router = createRouter({
--- src/views/MainCardSingle.vue
+++ src/views/MainCardSingle.vue
@@ -4,18 +4,21 @@ import { faker } from '@faker-js/faker'
 const firstname = faker.person.firstName();
 const lastname = faker.person.lastName();
 const fullname = `${firstname} ${lastname}`
+const username = faker.internet.userName({ firstName: firstname, lastName: lastname }).toLowerCase();
 const title = faker.person.jobTitle();
 const bio = faker.lorem.lines(2);
 </script>
 
 <template>
-    <div class="border border-gray-400 rounded-lg shadow bg-gray-200">
+    <RouterLink :to="`/employees/${username}`">
+        <div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full">
 
-        <img class="rounded-t-xl" :src="faker.image.urlLoremFlickr({ category: 'cat' })" />
-        <div class="p-4">
-            <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
-            <p class="mb-3 font-normal text-gray-700">{{ title }}</p>
-            <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+            <img class="rounded-t-xl" :src="faker.image.urlLoremFlickr({ category: 'cat' })" />
+            <div class="p-4">
+                <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
+                <p class="mb-3 font-normal text-gray-700">{{ title }}</p>
+                <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+            </div>
         </div>
-    </div>
+    </RouterLink>
 </template>
